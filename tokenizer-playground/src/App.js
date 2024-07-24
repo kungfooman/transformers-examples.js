dfd642b4b45aaf9c0b54d28c08a1968b90d5e69f
kungfooman <lama12345@gmail.com>
2024-07-24 08:00:34 +0200

    Make tokenizer-playground example buildfree

--- tokenizer-playground/src/App.js
+++ tokenizer-playground/src/App.js
@@ -1,5 +1,6 @@
-import {useCallback, useEffect, useRef, useState} from 'react';
-import {Token} from './components/Token';
+import {useCallback, useEffect, useRef, useState, createElement} from 'react';
+import {Token} from './components/Token.js';
+import {Worker} from 'worker-with-import-map';
 // Define list of tokenizers and their corresponding human-readable names
 const TOKENIZER_OPTIONS = Object.freeze({
   'Xenova/gpt-4': 'gpt-4 / gpt-3.5-turbo / text-embedding-ada-002',
@@ -16,8 +17,7 @@ const TOKENIZER_OPTIONS = Object.freeze({
   'Xenova/t5-small': 'T5',
   'Xenova/bert-base-cased': 'bert-base-cased',
   '': 'Custom',
-})
-
+});
 function App() {
   // Allow user to set tokenizer and text via URL query parameters
   const urlParams = new URLSearchParams(window.location.search);
@@ -92,84 +92,251 @@ function App() {
   }, []);
 
   return (
-    <div className='w-full max-w-[720px] flex flex-col gap-4 items-center'>
-
-      <div>
-        <h1 className='text-5xl font-bold mb-2'>The Tokenizer Playground</h1>
-        <h2 className='text-lg font-normal'>Experiment with different tokenizers (running <a className="text-gray-900 underline" href="https://github.com/xenova/transformers.js">locally</a> in your browser).</h2>
-      </div>
-
-      <div>
-        <select value={(tokenizer in TOKENIZER_OPTIONS && !customTokenizer) ? tokenizer : ''} onChange={(e) => {
-          resetOutput();
-          setCustomTokenizer('');
-          onTokenizerChange(e);
-        }} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2">
-          {Object.entries(TOKENIZER_OPTIONS).map(([value, label]) => (
-            <option key={value} value={value}>{label}</option>
-          ))}
-        </select>
-        {(!(tokenizer in TOKENIZER_OPTIONS) || customTokenizer || tokenizer === '') && (
-          <input
-            type="text"
-            placeholder="Custom tokenizer"
-            defaultValue={customTokenizer || tokenizer}
-            onChange={(e) => {
-              setCustomTokenizer(e.target.value);
+    createElement(
+      "div",
+      {
+        className: 'w-full max-w-[720px] flex flex-col gap-4 items-center',
+      },
+      createElement(
+        "div",
+        null,
+        createElement(
+          "h1",
+          {
+            className: 'text-5xl font-bold mb-2',
+          },
+          "The Tokenizer Playground",
+        ),
+        createElement(
+          "h2",
+          {
+            className: 'text-lg font-normal',
+          },
+          "Experiment with different tokenizers (running ",
+          createElement(
+            "a",
+            {
+              className: "text-gray-900 underline",
+              href: "https://github.com/xenova/transformers.js",
+            },
+            "locally",
+          ),
+          " in your browser).",
+        ),
+      ),
+      createElement(
+        "div",
+        null,
+        createElement(
+          "select",
+          {
+            value: (tokenizer in TOKENIZER_OPTIONS && !customTokenizer) ? tokenizer : '',
+            onChange: (e) => {
+              resetOutput();
+              setCustomTokenizer('');
               onTokenizerChange(e);
-            }}
-            className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-1 px-2 mt-1"
-          />
-        )}
-      </div>
-
-
-      <textarea
-        ref={textareaRef}
-        onChange={onInputChange}
-        rows="8"
-        className="font-mono text-lg block w-full p-2.5 text-gray-900 bg-gray-50 rounded-lg border border-gray-200"
-        placeholder="Enter some text"
-        defaultValue={textParam ?? textareaRef.current?.value ?? ''}
-      ></textarea>
-
-      <div className='flex justify-center gap-5'>
-        <div className='flex flex-col'>
-          <h2 className='font-semibold uppercase leading-4'>Tokens</h2>
-          <h3 className='font-semibold text-3xl'>{tokenIds.length.toLocaleString()}</h3>
-        </div>
-        <div className='flex flex-col'>
-          <h2 className='font-semibold uppercase leading-4'>Characters</h2>
-          <h3 className='font-semibold text-3xl'>{(textareaRef.current?.value.length ?? 0).toLocaleString()}</h3>
-        </div>
-      </div>
-
-      <div ref={outputRef} className='font-mono text-lg p-2.5 w-full bg-gray-100 rounded-lg border border-gray-200 whitespace-pre-wrap text-left h-[200px] overflow-y-auto'>
-        {outputOption === 'text' ? (
-          decodedTokens.map(
-            (token, index) => <Token key={index} text={token} position={index} margin={margins[index]} />
+            },
+            className: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2",
+          },
+          Object.entries(TOKENIZER_OPTIONS).map(([value, label]) => {
+            return (
+              createElement(
+                "option",
+                {
+                  key: value,
+                  value,
+                },
+                label,
+              )
+            );
+          }),
+        ),
+        (!(tokenizer in TOKENIZER_OPTIONS) || customTokenizer || tokenizer === '') && (
+          createElement(
+            "input",
+            {
+              type: "text",
+              placeholder: "Custom tokenizer",
+              defaultValue: customTokenizer || tokenizer,
+              onChange: (e) => {
+                setCustomTokenizer(e.target.value);
+                onTokenizerChange(e);
+              },
+              className: "bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-1 px-2 mt-1",
+            }
           )
+        ),
+      ),
+      createElement(
+        "textarea",
+        {
+          ref: textareaRef,
+          onChange: onInputChange,
+          rows: "8",
+          className: "font-mono text-lg block w-full p-2.5 text-gray-900 bg-gray-50 rounded-lg border border-gray-200",
+          placeholder: "Enter some text",
+          defaultValue: textParam ?? textareaRef.current?.value ?? '',
+        }
+      ),
+      createElement(
+        "div",
+        {
+          className: 'flex justify-center gap-5',
+        },
+        createElement(
+          "div",
+          {
+            className: 'flex flex-col',
+          },
+          createElement(
+            "h2",
+            {
+              className: 'font-semibold uppercase leading-4',
+            },
+            "Tokens",
+          ),
+          createElement(
+            "h3",
+            {
+              className: 'font-semibold text-3xl',
+            },
+            tokenIds.length.toLocaleString(),
+          ),
+        ),
+        createElement(
+          "div",
+          {
+            className: 'flex flex-col',
+          },
+          createElement(
+            "h2",
+            {
+              className: 'font-semibold uppercase leading-4',
+            },
+            "Characters",
+          ),
+          createElement(
+            "h3",
+            {
+              className: 'font-semibold text-3xl',
+            },
+            (textareaRef.current?.value.length ?? 0).toLocaleString(),
+          ),
+        ),
+      ),
+      createElement(
+        "div",
+        {
+          ref: outputRef,
+          className: 'font-mono text-lg p-2.5 w-full bg-gray-100 rounded-lg border border-gray-200 whitespace-pre-wrap text-left h-[200px] overflow-y-auto',
+        },
+        outputOption === 'text' ? (
+          decodedTokens.map((token, index) => {
+            return createElement(
+              Token,
+              {
+                key: index,
+                text: token,
+                position: index,
+                margin: margins[index],
+              }
+            );
+          })
         ) : outputOption === 'token_ids' ? (
           `[${tokenIds.join(', ')}]`
-        ) : null}
-      </div>
-
-      <div className="flex items-center gap-2 self-end">
-        <div className="flex items-center">
-          <input checked={outputOption === 'text'} onChange={() => setOutputOption('text')} id="output-radio-1" type="radio" value="" name="output-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
-          <label htmlFor="output-radio-1" className="ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">Text</label>
-        </div>
-        <div className="flex items-center">
-          <input checked={outputOption === 'token_ids'} onChange={() => setOutputOption('token_ids')} id="output-radio-2" type="radio" value="" name="output-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
-          <label htmlFor="output-radio-2" className="ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">Token IDs</label>
-        </div>
-        <div className="flex items-center">
-          <input checked={outputOption === null} onChange={() => setOutputOption(null)} id="output-radio-3" type="radio" value="" name="output-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
-          <label htmlFor="output-radio-3" className="ml-1 text-sm font-medium text-gray-900 dark:text-gray-300">Hide</label>
-        </div>
-      </div>
-    </div >
-  )
+        ) : null,
+      ),
+      createElement(
+        "div",
+        {
+          className: "flex items-center gap-2 self-end",
+        },
+        createElement(
+          "div",
+          {
+            className: "flex items-center",
+          },
+          createElement(
+            "input",
+            {
+              checked: outputOption === 'text',
+              onChange: () => {
+                return setOutputOption('text');
+              },
+              id: "output-radio-1",
+              type: "radio",
+              value: "",
+              name: "output-radio",
+              className: "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500",
+            }
+          ),
+          createElement(
+            "label",
+            {
+              htmlFor: "output-radio-1",
+              className: "ml-1 text-sm font-medium text-gray-900 dark:text-gray-300",
+            },
+            "Text",
+          ),
+        ),
+        createElement(
+          "div",
+          {
+            className: "flex items-center",
+          },
+          createElement(
+            "input",
+            {
+              checked: outputOption === 'token_ids',
+              onChange: () => {
+                return setOutputOption('token_ids');
+              },
+              id: "output-radio-2",
+              type: "radio",
+              value: "",
+              name: "output-radio",
+              className: "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500",
+            }
+          ),
+          createElement(
+            "label",
+            {
+              htmlFor: "output-radio-2",
+              className: "ml-1 text-sm font-medium text-gray-900 dark:text-gray-300",
+            },
+            "Token IDs",
+          ),
+        ),
+        createElement(
+          "div",
+          {
+            className: "flex items-center",
+          },
+          createElement(
+            "input",
+            {
+              checked: outputOption === null,
+              onChange: () => {
+                return setOutputOption(null);
+              },
+              id: "output-radio-3",
+              type: "radio",
+              value: "",
+              name: "output-radio",
+              className: "w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500",
+            }
+          ),
+          createElement(
+            "label",
+            {
+              htmlFor: "output-radio-3",
+              className: "ml-1 text-sm font-medium text-gray-900 dark:text-gray-300",
+            },
+            "Hide",
+          ),
+        ),
+      ),
+    )
+  );
 }
-
-export default App
+export {App};
--- tokenizer-playground/src/components/Token.js
+++ tokenizer-playground/src/components/Token.js
@@ -1,23 +1,42 @@
-import { Fragment } from 'react';
-
+import {createElement, Fragment} from 'react';
 const COLOURS = [
-    'bg-purple-300',
-    'bg-green-300',
-    'bg-yellow-300',
-    'bg-red-300',
-    'bg-blue-300',
-]
-
-export function Token({ text, position, margin }) {
-    const textWithLineBreaks = text.split('\n').map((line, index, array) => (
-        <Fragment key={index}>
-            {line}
-            {index !== array.length - 1 && <br />}
-        </Fragment>
-    ));
-    return (<span
-        style={{ marginLeft: margin }}
-        className={`leading-5 ${textWithLineBreaks.length === 1 ? 'inline-block ' : ''}${COLOURS[position % COLOURS.length]}`}>
-        {textWithLineBreaks}
-    </span>)
+  'bg-purple-300',
+  'bg-green-300',
+  'bg-yellow-300',
+  'bg-red-300',
+  'bg-blue-300',
+];
+function Token({
+  text,
+  position,
+  margin
+}) {
+  const textWithLineBreaks = text.split('\n').map((line, index, array) => {
+    return (
+      createElement(
+        Fragment,
+        {
+          key: index,
+        },
+        line,
+        index !== array.length - 1 && createElement(
+          "br",
+          null
+        ),
+      )
+    );
+  });
+  return (
+    createElement(
+      "span",
+      {
+        style: {
+          marginLeft: margin
+        },
+        className: `leading-5 ${textWithLineBreaks.length === 1 ? 'inline-block ' : ''}${COLOURS[position % COLOURS.length]}`,
+      },
+      textWithLineBreaks,
+    )
+  );
 }
+export {Token};
